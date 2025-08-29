22de2ba926c850df8c5eb4c6ca516b83ad2eca6d
ivan280809 <ivanrobertomaneroii7@gmail.com>
2025-08-30 00:42:02 +0200

    refactor NorwegianBlueParrot class to improve speed calculation and cry sound logic

--- src/main/java/dev/kata/parrot/models/NorwegianBlueParrot.java
+++ src/main/java/dev/kata/parrot/models/NorwegianBlueParrot.java
@@ -3,8 +3,10 @@ package dev.kata.parrot.models;
 public class NorwegianBlueParrot implements ParrotSpecies {
 
     private static final double BASE_SPEED = 12.0;
+    private static final double MIN_SPEED = 24.0;
     private static final String CRY_SOUND = "Bzzzzzz";
     private static final String DEAD_CRY_SOUND = "...";
+    private static final double NO_SPEED = 0;
 
     private final double voltage;
     private final boolean isNailed;
@@ -16,15 +18,15 @@ public class NorwegianBlueParrot implements ParrotSpecies {
 
     @Override
     public double getSpeed() {
-        return (isNailed) ? 0 : getBaseSpeed(voltage);
+        return (isNailed) ? NO_SPEED : getBaseSpeed(voltage);
     }
 
     @Override
     public String getCry() {
-        return voltage > 0 ? CRY_SOUND : DEAD_CRY_SOUND;
+        return voltage > NO_SPEED ? CRY_SOUND : DEAD_CRY_SOUND;
     }
 
     private double getBaseSpeed(double voltage) {
-        return Math.min(24.0, voltage * BASE_SPEED);
+        return Math.min(MIN_SPEED, voltage * BASE_SPEED);
     }
 }
